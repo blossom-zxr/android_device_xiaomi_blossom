7fca178214e3c88b3baa2d56630b19e7a7df5c76
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    blossom: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -149,6 +149,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.blossom

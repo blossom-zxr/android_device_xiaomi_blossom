a93130d54d04decd4b5f8f49ca27f18e630efaf4
bengris32 <bengris32@protonmail.ch>
2023-07-21 13:10:34 +0100

    blossom: Ship MediaTek cgroup/task_profiles configuration
    * MediaTek ship their own modified task_profiles and
      use the V30 cgroups configuration.
    
    Change-Id: I15245ea87b00925597db65f1564e808a094c937a
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -441,6 +441,11 @@ PRODUCT_PACKAGES += \
     libshim_showlogo \
     libshim_vtservice
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # VNDK
 PRODUCT_PACKAGES += \
     libui-v32

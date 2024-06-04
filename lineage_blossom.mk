cdf436d90d8f4dc0ead163281551dca7fade65cb
fukiame <fukiame@proton.me>
2024-06-04 16:12:11 +0700

    blossom: include private signing keys
    それでも雨は降るんだね
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -15,6 +15,9 @@ $(call inherit-product, device/xiaomi/blossom/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# include private signing keys
+-include vendor/niigo-priv/keys/keys.mk
+
 PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi

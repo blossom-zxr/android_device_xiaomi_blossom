53712564760f5726f3606aa3eb0fa37d6182aa9b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-22 16:11:42 +0800

    blossom: Use the proper way to disable console service
    Change-Id: Ie5d1c7c9b4937eaeb0c279b2427ddae23bc9f4a7
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: fukiame <fukiame@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,7 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += androidboot.serialconsole=0
 BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_CMDLINE += quiet loglevel=3
 BOARD_KERNEL_CMDLINE += cgroup_disable=pressure

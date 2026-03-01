a3455823bc0005918c3033dd0ce4a2cf49d31b7a
fukiame <fukiame@proton.me>
2026-03-02 03:39:23 +0700

    blossom: init: variants: override build flavor
    Inspired-by: https://github.com/MistOS-psyche/device_xiaomi_psyche/commit/d0eb2e975ba3d2f7ede00a3fdaff08f1b77fb33a
    Signed-off-by: fukiame <fukiame@proton.me>

--- init/variants.cpp
+++ init/variants.cpp
@@ -25,4 +25,5 @@ void set_variant_props(const variant_info_t variant) {
     property_override("bluetooth.device.default_name", variant.marketname);
     property_override("vendor.usb.product_string", variant.marketname);
     property_override("ro.boot.hardware.sku", variant.device);
+    property_override("ro.build.flavor", variant.mod_device + "-user");
 }

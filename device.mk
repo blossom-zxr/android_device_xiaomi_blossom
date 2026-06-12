0de7973cf09e88591383acc91d0a4b52b435dac5
fukiame <fukiame@proton.me>
2026-06-13 03:40:40 +0700

    blossom: rro_overlays: kang los sdk overlay from los FP5
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -511,6 +511,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Runtime Resource Overlays
 PRODUCT_PACKAGES += \
+    LineageSdkOverlayBlossom \
     FrameworksResOverlayBlossom \
     SettingsOverlayBlossom \
     SettingsProviderOverlayBlossom \
